e8379314ff533e90066fb0549a9fffcd154dcbe4
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2025-02-13 15:27:37 +0500

    new update

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -3,7 +3,7 @@ import { Navbar } from "../components/navbar/navbar";
 import "./layout.css";
 import "../components/component.css";
 import music from "../assets/music.mp3";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RiMusicAiFill } from "react-icons/ri";
 import Message from "../components/message";
 import bg from "../assets/v.jpg";
@@ -11,18 +11,26 @@ import { CitizensModal, InventoryModal } from "../components/modal";
 import { useSelector } from "react-redux";
 
 export const Layout = () => {
-  const [audio] = useState(new Audio(music));
-  const { citizen = false, inventory = false } = useSelector(
-    (state) => state.modals
-  );
-  console.log(inventory);
+ const [audio] = useState(new Audio(music));
+ const [isMusicStarted, setIsMusicStarted] = useState(false);
+ const { citizen = false, inventory = false } = useSelector(
+   (state) => state.modals
+ );
 
-  // const playMusic = () => {
-  //   audio.loop = true;
-  //   // volume is between 0 and 1
-  //   audio.volume = 0.1;
-  //   audio.play();
-  // };
+ useEffect(() => {
+   const playMusic = () => {
+     if (!isMusicStarted) {
+       audio.loop = true;
+       audio.volume = 0.1;
+       audio.play().catch((err) => console.log("Autoplay blocked:", err));
+       setIsMusicStarted(true);
+     }
+   };
+
+   document.addEventListener("click", playMusic, { once: true });
+
+   return () => document.removeEventListener("click", playMusic);
+ }, [isMusicStarted, audio]);
 
   const toggleMusic = () => {
     if (audio.paused) {
